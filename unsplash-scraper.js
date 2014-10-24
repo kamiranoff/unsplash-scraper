6c470611906c823efc2a15c8c3cf8c5663717626
Jeff Holman <jeff@c16y.com>
2014-10-23 22:17:54 -0700

    some enhancements

--- unsplash-scraper.js
+++ unsplash-scraper.js
@@ -1,30 +1,57 @@
 var request = require('request');
+var requestretry = require('requestretry');
 var cheerio = require('cheerio');
 var url = require('url');
 var exec = require('child_process').exec;
+var fs = require('fs');
+
+function getUnsplashImage(host, hostURL, dlDir, dlPage, tryNum) {
+	requestretry({
+  		url: host+dlPage,
+  		json:true,
+
+  		// The above parameters are specific to Request-retry:
+  		maxAttempts: 5,   // (default) try 5 times
+  		retryDelay: 5000  // (default) wait for 5s before trying again
+	},function(err,resp,body){
+                //redirects image
+		if (dlPage.indexOf("download") > -1 && resp) {
+                	var fileUrl = resp.request.uri.href.split('?').shift();
+                	var fileName = url.parse(fileUrl).pathname.split('/').pop().split('?').shift();
+			console.log("Started: " + fileUrl);
+
+                	//in curl we have to escape '&' from fileUrl
+			if (!fs.existsSync(dlDir+fileName)) {
+				if (fileName.indexOf(".jpg") == -1) fileName = fileName + ".jpg";
+                		var curl =  'curl ' + fileUrl.replace(/&/g,'\\&') +' -o ' + dlDir+fileName + ' --create-dirs';
+                		var child = exec(curl, function(err, stdout, stderr) {
+                   	 		if (err){ 
+						fs.unlinkSync(dlDir+fileName);
+						console.log("Failed: " + fileUrl + " Try: " + tryNum);
+						if (tryNum <= 5) {
+							getUnsplashImage(host, hostURL, dlDir, dlPage, tryNum+1) 
+						}
+					} 
+                   	 		else console.log("Finished: " + fileName);
+                		});
+			}
+		}
+            })
+};
 
 (function() {
     var dlDir = './downloads/';
-    var host = 'http://unsplash.com';
+    var hostURL = 'https://unsplash.com/grid';
+    var host = 'https://unsplash.com';
     
-    request(host, function(err, resp, body) {
+    request(hostURL, function(err, resp, body) {
         if (err) throw err;
 
         $ = cheerio.load(body);
         $('.photo a').each(function(){
             var dlPage = $(this).attr('href');
-
-            request(host+dlPage,function(err,resp,body){
-                //redirects image
-                var fileUrl = resp.request.uri.href;
-                var fileName = url.parse(fileUrl).pathname.split('/').pop().split('?').shift();
-                //in curl we have to escape '&' from fileUrl
-                var curl =  'curl ' + fileUrl.replace(/&/g,'\\&') +' -o ' + dlDir+fileName + ' --create-dirs';
-                var child = exec(curl, function(err, stdout, stderr) {
-                    if (err){ console.log(stderr); throw err; } 
-                    else console.log(fileName + ' downloaded to ' + dlDir);
-                });
-            })
+            setTimeout(getUnsplashImage(host, hostURL, dlDir, dlPage, 1), 5000);
         });        
     });
 })();
+
